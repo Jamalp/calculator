114b9d268c452619c7a0513db92cc597eed6dc53
Jamal Powell <jamalkpowell@gmail.com>
2013-06-18 20:29:05 -0400

    Fix the New Menu

--- Menu2.rb
+++ Menu2.rb
@@ -13,13 +13,34 @@ PUDDING = 6
 
 total = 0
 
+puts "What do you want to start with?"
+puts "we have bread or cheese"
+appetizer = gets.chomp
 case appetizer
+
 when "bread"
   total += BREAD
 when "cheese"
   total +=CHEESE
 end
 
+puts "what do you want to eat for a main?"
+puts "we have steak or salmon. Make the right choice!"
+entree = gets.chomp
 case entree
 when "steak"
   total += STEAK
+when "cheese"
+  total += CHEESE
+end
+
+puts "Ok, and for dessert? We've got cake or pudding"
+dessert = gets.chomp
+case dessert
+when "cake"
+  total += CAKE
+when "pudding"
+  total += PUDDING
+end
+
+puts "you're total is #{total}".to_s
